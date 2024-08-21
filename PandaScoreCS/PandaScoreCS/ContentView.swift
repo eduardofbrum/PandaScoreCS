50ca91ec4ddc2f4b5f6cb7bbc7d3436b7118a6d7
Eduardo Brum <eduardofilot@hotmail.com>
2024-08-21 10:52:59 -0300

    create new project

---
+++ PandaScoreCS/PandaScoreCS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PandaScoreCS
+//
+//  Created by Eduardo Filot Brum on 21/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PandaScoreCS/PandaScoreCS/PandaScoreCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaScoreCSApp.swift
+//  PandaScoreCS
+//
+//  Created by Eduardo Filot Brum on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PandaScoreCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
