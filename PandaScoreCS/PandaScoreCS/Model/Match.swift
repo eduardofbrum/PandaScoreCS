08829898b49497fa119d66f92290573cb43c5628
Eduardo Brum <eduardofilot@hotmail.com>
2024-08-27 10:40:48 -0300

    create match fixture

--- PandaScoreCS/PandaScoreCS/Model/Match.swift
+++ PandaScoreCS/PandaScoreCS/Model/Match.swift
@@ -1,19 +1,35 @@
-struct Match: Decodable, Identifiable {
-    let id: Int
-    let status: MatchStatus
-    let scheduledAt: String?
+public struct Match: Decodable, Identifiable {
+    public let id: Int
+    public let status: MatchStatus
+    public let scheduledAt: String?
     
-    let league: League
-    let opponents: [Opponent]?
-    let serie: Serie
+    public let league: League
+    public let opponents: [Opponent]?
+    public let serie: Serie
     
     enum CodingKeys: String, CodingKey {
         case id, status, league, opponents, serie
         case scheduledAt = "scheduled_at"
     }
+    
+    public init(
+        id: Int,
+        status: MatchStatus,
+        scheduledAt: String?,
+        league: League,
+        opponents: [Opponent]?,
+        serie: Serie
+    ) {
+        self.id = id
+        self.status = status
+        self.scheduledAt = scheduledAt
+        self.league = league
+        self.opponents = opponents
+        self.serie = serie
+    }
 }
 
-enum MatchStatus: String, Codable {
+public enum MatchStatus: String, Codable {
     case notStarted = "not_started"
     case running
     case finished
@@ -21,22 +37,27 @@ enum MatchStatus: String, Codable {
     case postponed
 }
 
-struct League: Decodable {
-    let name: String
-    let imageUrl: String?
+public struct League: Decodable {
+    public let name: String
+    public let imageUrl: String?
     
     enum CodingKeys: String, CodingKey {
         case name
         case imageUrl = "image_url"
     }
+    
+    public init(name: String, imageUrl: String?) {
+        self.name = name
+        self.imageUrl = imageUrl
+    }
 }
 
-struct Opponent: Decodable {
+public struct Opponent: Decodable {
     let opponent: Team
     let type: String
 }
 
-struct Team: Decodable {
+public struct Team: Decodable {
     let id: Int
     let name: String
     let imageUrl: String?
@@ -47,10 +68,34 @@ struct Team: Decodable {
     }
 }
 
-struct Serie: Decodable {
-    let name: String
+public struct Serie: Decodable {
+    public let name: String
 
     enum CodingKeys: String, CodingKey {
         case name
+    }
+    
+    public init(name: String) {
+        self.name = name
+    }
+}
+
+public extension Match {
+    static func fixture(
+        id: Int = 1,
+        status: MatchStatus = .notStarted,
+        scheduleAt: String? = "",
+        league: League = .init(name: "League name", imageUrl: ""),
+        opponents: [Opponent]? = [],
+        serie: Serie = .init(name: "Serie name")
+    ) -> Self {
+        .init(
+            id: id,
+            status: status,
+            scheduledAt: scheduleAt,
+            league: league,
+            opponents: opponents,
+            serie: serie
+        )
     }
 }
